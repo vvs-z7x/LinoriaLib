5430a9bc3085e8228df1124eb7153aa4f025dd02
vvs-z7x <vvs.z7x@gmail.com>
2026-01-11 04:05:30 -0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,3 +1,7 @@
+if getgenv().Library then
+    getgenv().Library:Unload()
+end
+
 local InputService = game:GetService('UserInputService');
 local TextService = game:GetService('TextService');
 local CoreGui = game:GetService('CoreGui');
@@ -2948,7 +2952,7 @@ function Library:CreateWindow(...)
     if type(Config.MenuFadeTime) ~= 'number' then Config.MenuFadeTime = 0.2 end
 
     if typeof(Config.Position) ~= 'UDim2' then Config.Position = UDim2.fromOffset(175, 50) end
-    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(850, 600) end
+    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(1001, 600) end
 
     if Config.Center then
         Config.AnchorPoint = Vector2.new(0.5, 0.5)
