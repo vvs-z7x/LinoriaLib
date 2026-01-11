ca5f7db5d37c2a9c477d92521ffba779e00c12fa
vvs-z7x <vvs.z7x@gmail.com>
2026-01-11 04:06:33 -0800

    Change default Config.Size dimensions

--- Library.lua
+++ Library.lua
@@ -2952,7 +2952,7 @@ function Library:CreateWindow(...)
     if type(Config.MenuFadeTime) ~= 'number' then Config.MenuFadeTime = 0.2 end
 
     if typeof(Config.Position) ~= 'UDim2' then Config.Position = UDim2.fromOffset(175, 50) end
-    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(1001, 600) end
+    if typeof(Config.Size) ~= 'UDim2' then Config.Size = UDim2.fromOffset(850, 600) end
 
     if Config.Center then
         Config.AnchorPoint = Vector2.new(0.5, 0.5)
